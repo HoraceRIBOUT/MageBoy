be66081870e43ca2d28031a68bfe15a081cbbacc
RIBOUT <pifi77@hotmail.fr>
2020-09-09 14:31:24 +0200

    [Level Design Tools] Add a name

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -32,6 +32,7 @@ public class LevelManager : MonoBehaviour
     [System.Serializable]
     public struct Level
     {
+        public string id;
         public List<GridEntity.GridElement> entityOnThisLevel;
     }
 
